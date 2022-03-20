db07df520e74bea35558390fd842404dc36b7e9c
Erin Jackson <erinebjackson@gmail.com>
2022-03-19 20:12:13 -0700

    adds bootstrap and design to front end

--- src/App.vue
+++ src/App.vue
@@ -17,46 +17,45 @@ export default {
 </script>
 
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link v-if="!isLoggedIn" to="/signup">Signup</router-link>
-    |
-    <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
-    |
-    <router-link v-if="!!isLoggedIn" to="/logout">Logout</router-link>
-    |
-    <router-link v-if="!!isLoggedIn" to="/users/me">View Account</router-link>
-    |
-    <router-link v-if="!!isLoggedIn" to="/workouts/me">My Workouts</router-link>
-    |
-    <router-link v-if="!!isLoggedIn" to="/workoutcreate">New Workout</router-link>
-    |
-    <router-link v-if="!!isLoggedIn" to="/workoutsort">Exercise Filter</router-link>
-    |
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">What Workout?</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link to="/" class="nav-link workout">Home</router-link>
+          <router-link v-if="!isLoggedIn" to="/signup" class="nav-link active" aria-current="page">Signup</router-link>
+          <router-link v-if="!isLoggedIn" to="/login" class="nav-link workout">Login</router-link>
+          <router-link v-if="!!isLoggedIn" to="/logout" class="nav-link workout">Logout</router-link>
+          <router-link v-if="!!isLoggedIn" to="/users/me" class="nav-link workout">View Account</router-link>
+          <router-link v-if="!!isLoggedIn" to="/workouts/me" class="nav-link workout">My Workout</router-link>
+          <router-link v-if="!!isLoggedIn" to="/workoutcreate" class="nav-link workout">New workout</router-link>
+          <router-link v-if="!!isLoggedIn" to="/workoutsort" class="nav-link workout">Exercise Filter</router-link>
+        </div>
+        <div class="right-nav-item">
+          <router-link v-if="!!isLoggedIn" to="/workoutsort" class="nav-link workout btn float-end">
+            Quick Workout
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+
   <router-view />
+
+  <footer>
+    <h4>- What Workout? -</h4>
+  </footer>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -71,60 +71,145 @@ export default {
 
 <template>
   <div class="exercises">
-    <h1>What Workout?</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat.
-    </p>
-    <div class="get-workout">
-      <h2>Quick Workout</h2>
-      <p>
-        Just want a quick workout you aren't gonna save? Just enter the muscle group and number of exercises and you can
-        start your workout! Be sure to sign in if you want to have the option to save your workout.
-      </p>
-      <form ref="formSave" @submit="submitForm" v-on:submit.prevent="getWorkout">
-        <label for="target">Choose a Muscle Group</label><br>
-        <select v-model="target">
-          <option v-for="target in targets" v-bind:key="target">
-            {{ target }}
-          </option>
-        </select>
-        <br />
-        <!-- <input type="text" v-model="target" placeholder="Muscle Group" />
+    <div class="home-hero">
+      <b-jumbotron header="WhatWorkout?" lead="Bootstrap v4 Components for Vue.js 2">
+        <div class="row align-items-center">
+          <div class="col-5">
+            <div class="hero-card">
+              <b-card title="Card Title" tag="article" class="mb-2">
+                <h1>What Workout?</h1>
+                <b-card-text class="card-content">
+                  Take the guess work out of your workout. If it's leg day, we got some leg moves. Need some ab work?
+                  We've got a few hundred exercises. No more thinking about what to do!
+                </b-card-text>
+                <br />
+                <button class="card-content"><a href="#get-started" variant="primary">Get Started</a></button>
+              </b-card>
+            </div>
+          </div>
+        </div>
+
+        <b-button variant="primary" href="#">More Info</b-button>
+      </b-jumbotron>
+    </div>
+    <div class="container">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-8 align-items-center">
+            <h2>How It Works</h2>
+            <p>
+              We take the guess work out of what to do for your workout today. Just decide what muscle group you want to
+              target, the number of exercises you want, and hit "Get A Workou". Our database will give you a list of
+              workouts along with a helpful gif to get you started. If you want to be able to save your workouts for
+              future use, be sure to make an account.
+            </p>
+            <button><router-link to="/signup">Sign Up</router-link></button>
+          </div>
+        </div>
+      </div>
+      <!-- <b-container class="bv-example-row">
+        <b-row class="justify-content-md-center">
+          <b-col cols="8" md="auto">
+            <h1>How It Works</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat.
+            </p>
+          </b-col>
+        </b-row>
+      </b-container> -->
+      <div class="container">
+        <div class="row center">
+          <div class="col-4">
+            <div class="get-workout" id="get-started">
+              <h3>Quick Workout</h3>
+              <p>
+                Just want a quick workout you aren't gonna save? Just enter the muscle group and number of exercises and
+                you can start your workout! Be sure to sign in if you want to have the option to save your workout.
+              </p>
+            </div>
+          </div>
+          <div class="col-4 float-right">
+            <form class="workout-form" ref="formSave" @submit="submitForm" v-on:submit.prevent="getWorkout">
+              <label for="target"><h3>Choose a Muscle Group</h3></label>
+              <br />
+              <select v-model="target">
+                <option v-for="target in targets" v-bind:key="target">
+                  {{ target }}
+                </option>
+              </select>
+              <br />
+              <!-- <input type="text" v-model="target" placeholder="Muscle Group" />
         <br /> -->
 
-        <input type="integer" v-model="exercise_count" placeholder="Number of Exercises" />
+              <input type="integer" v-model="exercise_count" placeholder="Number of Exercises" />
 
-        <br />
-        <input v-on:click="show = true" type="submit" value="Get A Workout" />
-        <p v-if="show">If you don't like these exercises, just try again.</p>
-      </form>
-    </div>
-    <br />
-    <div v-if="show">
-      <h3>{{ message }}</h3>
-      Muscle Group: {{ target }}
-      <br />
-      <br />
-      <div v-for="exercise in exercises" v-bind:key="exercise">
-        <br />
-        <img v-bind:src="exercise.gifUrl" />
-        <br />
-        # {{ exercise.id }}
-        <br />
-        Exercise Name: {{ exercise.name }}
-        <br />
-        Equipment: {{ exercise.equipment }}
-        <br />
-        Bodypart: {{ exercise.bodyPart }}
-        <br />
-        Target Muscle: {{ exercise.target }}
+              <br />
+              <input v-on:click="show = true" type="submit" value="Get A Workout" class="input-button" />
+              <p v-if="show" class="small-text center-text">
+                If you don't like these exercises,
+                <br />
+                just try again.
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-      <div v-if="!isLoggedIn" class="signup-link">
-        <h4>Want to save a workout? Sign up for an account and save your workouts</h4>
-        <router-link to="/signup"><button>Create Account</button></router-link>
+
+      <br />
+      <div class="center-text">
+        <h3>{{ message }}</h3>
+        <h4>Muscle Group: {{ target }}</h4>
       </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card" v-for="exercise in exercises" v-bind:key="exercise">
+            <img class="card-img-top" v-bind:src="exercise.gifUrl" />
+            <div class="card-body">
+              <h5 class="card-title">Exercise Name: {{ exercise.name }}</h5>
+              <p class="card-text">
+                # {{ exercise.id }}
+                <br />
+                Equipment: {{ exercise.equipment }}
+                <br />
+                Bodypart: {{ exercise.bodyPart }}
+                <br />
+                Target Muscle:
+                {{ exercise.target }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div v-if="!isLoggedIn" class="signup-link">
+          <h4>Want to save a workout? Sign up for an account and save your workouts</h4>
+          <router-link to="/signup"><button>Create Account</button></router-link>
+        </div>
+      </div>
+
+      <!-- <div v-if="show">
+        Muscle Group: {{ target }}
+        <br />
+        <br />
+        <div v-for="exercise in exercises" v-bind:key="exercise">
+          <br />
+          <img class="card-img-top" v-bind:src="exercise.gifUrl" />
+          <br />
+          # {{ exercise.id }}
+          <br />
+          Exercise Name: {{ exercise.name }}
+          <br />
+          Equipment: {{ exercise.equipment }}
+          <br />
+          Bodypart: {{ exercise.bodyPart }}
+          <br />
+          Target Muscle: {{ exercise.target }}
+        </div> -->
+      <!-- <div v-if="!isLoggedIn" class="signup-link">
+          <h4>Want to save a workout? Sign up for an account and save your workouts</h4>
+          <router-link to="/signup"><button>Create Account</button></router-link>
+        </div> -->
+      <!-- </div> -->
     </div>
   </div>
 </template>
