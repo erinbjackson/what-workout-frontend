cc4be80f6ed4b183c5e014ded358d9d1db7d1fdc
Erin Jackson <erinebjackson@gmail.com>
2022-03-23 14:23:06 -0700

    Fixes small bug

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -27,30 +27,38 @@ export default {
 <template>
   <div class="signup-page container padding-t">
     <div class="signup col-4">
-      <form v-on:submit.prevent="submit()">
-        <h1>Signup</h1>
-        <ul>
-          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-        </ul>
-        <div>
-          <input class="form-control" type="text" placeholder="Name" v-model="newUserParams.name" />
-        </div>
-        <div>
-          <input class="form-control" type="email" placeholder="Email" v-model="newUserParams.email" />
-        </div>
-        <div>
-          <input class="form-control" type="password" placeholder="Password" v-model="newUserParams.password" />
-        </div>
-        <div>
-          <input
-            class="form-control"
-            type="password"
-            placeholder="Password Confirmation"
-            v-model="newUserParams.password_confirmation"
-          />
-        </div>
-        <input class="input-button wide form-control" type="submit" value="Submit" />
-      </form>
+      <div>
+        <form v-on:submit.prevent="submit()">
+          <h1>Signup</h1>
+          <ul>
+            <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+          </ul>
+          <div>
+            <input class="form-control" type="text" placeholder="Name" v-model="newUserParams.name" />
+          </div>
+          <div>
+            <input class="form-control" type="email" placeholder="Email" v-model="newUserParams.email" />
+          </div>
+          <div>
+            <input class="form-control" type="password" placeholder="Password" v-model="newUserParams.password" />
+          </div>
+          <div>
+            <input
+              class="form-control"
+              type="password"
+              placeholder="Password Confirmation"
+              v-model="newUserParams.password_confirmation"
+            />
+          </div>
+          <input class="input-button wide form-control" type="submit" value="Submit" />
+        </form>
+        <br />
+        <p class="center-text">
+          Have an account?
+          <br />
+          <router-link to="/login">Sign In</router-link>
+        </p>
+      </div>
     </div>
   </div>
 </template>
